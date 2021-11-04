782d681e2f966e94c11f1e12dfc9cd53d5ba6186
Juan Diego Gómez <juandieruiz6@gmail.com>
2021-11-04 13:25:50 +0100

    commit#5 Configuracion Vista SideBar & Tracks

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -3,7 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 
 const routes: Routes = [
-  
+
+  // Llamaremos a los modulos que se van a renderizar en la vista principal( router-outlet )
+  {
+    path:'tracks',
+    loadChildren: () => import('@modules/tracks/tracks.module').then(m => m.TracksModule)
+  },
+  {
+    path:'favorites',
+    loadChildren: () => import('@modules/favorites/favorites.module').then(m => m.FavoritesModule)
+  },
+  {
+    path:'tracks',
+    loadChildren: () => import('@modules/history/history.module').then(m => m.HistoryModule)
+  },
 ];
 
 @NgModule({
