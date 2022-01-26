28edfb5c844f5e2c0bc8bba37b34a6ef07f8f32a
Juan Diego Gómez <77864382+Juandieruiz@users.noreply.github.com>
2022-01-26 21:12:39 +0100

    Login Formulario ts

--- src/app/modules/auth/pages/login-page/login-page.component.ts
+++ src/app/modules/auth/pages/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
+// padre = formGroup
+// hijo = formControl
+  formLogin: FormGroup = new FormGroup({});
 
   constructor() { }
 
   ngOnInit(): void {
+    this.formLogin = new FormGroup({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormControl('', [
+        Validators.required, 
+        Validators.minLength(6),
+        Validators.maxLength(12)
+      ])
+    });
   }
 
+  sendLogin(){
+    const body = this.formLogin.value;
+    console.log(body);
+  }
 }
